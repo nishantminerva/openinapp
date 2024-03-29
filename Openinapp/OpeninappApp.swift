a32724e625e41e42c1fa7f0d7b3de090df018c6b
Nishant <nishantminerva@gmail.com>
2024-03-29 13:30:58 +0530

    Initial Commit

---
+++ Openinapp/OpeninappApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpeninappApp.swift
+//  Openinapp
+//
+//  Created by Nishant Minerva on 29/03/24.
+//
+
+import SwiftUI
+
+@main
+struct OpeninappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
